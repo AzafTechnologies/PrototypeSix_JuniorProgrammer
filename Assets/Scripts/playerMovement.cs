e186a93218c9365dbd786b6a68b470e35aa27a62
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-25 12:41:02 +0530

    Add configurable player jump physics
    Introduces configurable jump force and gravity settings to the player movement script, with grounded checks preventing airborne jumps. The scene file was also updated to expose the new tuning values for gameplay balancing.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,13 +4,16 @@ using UnityEngine.InputSystem;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody playerRb;
-
     public InputAction jumpInput;
+    public float jumpForce = 15.0f;
+    public float gravityModifier = 2.0f;
+    public bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        
+
+        Physics.gravity *= gravityModifier;
 
         jumpInput.Enable();
     }
@@ -18,9 +21,17 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (jumpInput.triggered)
+        if (jumpInput.triggered && isGrounded)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
     }
 }
