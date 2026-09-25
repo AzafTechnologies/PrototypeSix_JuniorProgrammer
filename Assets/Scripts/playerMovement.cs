ab520fd2aa1b4e85172ebb3020cc27bdca705acf
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-25 12:16:08 +0530

    Enable Input System jump input
    This change switches the project to the Input System backend and adds a Jump Input action bound to the Space key. The player movement script enables the input action during startup so jumping can be handled through the new action system.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody playerRb;
+
+    public InputAction jumpInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerRb.AddForce(Vector3.up * 1000);
+
+        jumpInput.Enable();
     }
 
     // Update is called once per frame
